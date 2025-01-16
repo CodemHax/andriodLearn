3a071baa944bf60c7a224431147b8b682a045645
codem <altaf.badure.it@ghrcem.raisoni.net>
2025-01-16 18:52:46 +0530

    Update main.kt

--- day1/main.kt
+++ day1/main.kt
@@ -15,3 +15,61 @@ fun main(){
 
 }
 
+// functions
+fun add(a: Int, b : Int) : Int{
+    val result :Int = a + b
+    return result
+}
+
+fun sub(a: Int, b : Int) : Int{
+    val result :Int = a - b
+    return result
+}
+fun mul(a: Int, b : Int) : Int{
+    val result :Int = a * b
+    return result
+}
+
+fun div(a: Int, b : Int) : Int{
+    val result :Int = a / b
+    return result
+}
+
+fun main(){
+    print("First Value\n")
+    val a : Int = readln().toInt()
+    print("Second Value\n")
+    val b : Int = readln().toInt()
+    println("Select the operation\n")
+    val operation = readln()
+
+    if (operation == "+"){
+        println(add(a,b))
+    }
+    else if (operation == "-"){
+       println(sub(a,b))
+    }
+    else if (operation == "/"){
+        println(div(a,b))
+    }
+    if (operation == "+"){
+        println(mul(a,b))
+    }
+
+}
+
+
+
+
+fun main() {
+    val petHunger = 50
+    val petEnergy = 70
+    val petHappiness = 60
+
+    println("Hunger: ${petHunger - 20}")
+    println("Energy: ${petEnergy + 40}")
+    println("Happiness: ${petHappiness + 30}")
+}
+
+
+

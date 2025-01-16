09c7e10d89be24531554b250f5fb4980bfe0f616
codem <altaf.badure.it@ghrcem.raisoni.net>
2025-01-16 18:34:25 +0530

    Update main.kt

--- day1/main.kt
+++ day1/main.kt
@@ -1,1 +1,17 @@
 //Strings
+fun main(){
+    val string : String = "Hello World"
+    println(" Length of String: " +  string.length )
+    for (i in 0..<string.length - 1)
+    println("Index Function: " + "${string[i]}")
+    println("Substring Function: " + string.substring(6,11))
+    println("UpperCase: " + string.uppercase())
+    println("LowerCase: " + string.lowercase())
+    println(string.trim())
+    println(string.contains("Hello"))
+    println(string.replace("o","O"))
+    println(string.split(" "))
+    println( "$" + "OK")
+
+}
+
